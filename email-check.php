7cc853e02f7f2f226b0077ee3745da02be458a76
rgbkad <kaderarnold@icloud.com>
2020-07-12 05:02:45 -0400

    more .php work, this time with configuring the email chain. we added a file to make sure the email our user submitted is not already in the database. if it was, we would potentially run into problems if a user was on the list twice. maybe it would increase the chances of both emails ending up caught in the spam folder. either way we dont want our automatic email module portion of the website to email one user two times, that would look bad.

--- email-check.php
+++ email-check.php
@@ -1,10 +1,32 @@
 <?php 
 
-// recieve all the email objects from mysql
-// run the potential email against all the email
-// objects and see if any have the same name
-// if there is a duplicate, show an alert, if there is not, call insert pair
+// so now literally all we have to do is query mysql using the $link object from out connection and test our post objects against the other submitted fields in an array
+// so lets make an array that contains all of the mysql database table's email fields  
 
+$email = $_POST['email'];
+$fullname = $_POST['fullname'];
+
+$sql = "show db customers email *;";
+$emailList = $result -> $mysqli -> $query($sql);
+
+for (i = 0; i < emailList.length; i++) {
+
+	var e  = emailList[i];	
+	var l = e.length;
+	var subEmail = ".$email.";
+	if (subEmail.length == l) {
+	
+		if (subEmail == e) {
+		
+			".window.alert('email is already in the database. we sent you an email just to reconfirm.');"
+		
+		}
+		// else add the email and fullname to the database tables empty fields
+		// make a new customer and feed it VALUES($fullname, $email)
+	}
+	
+
+}
 
 // this call is in between importing 'connect.php' and importing 'close.php'
 // so we just have to query the (assumingly open) database 
