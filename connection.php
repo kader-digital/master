abf289c2d377bcd3a6d1fe09c3b9ed219f2015e4
rgbkad <kaderarnold@icloud.com>
2020-07-12 03:16:05 -0400

    adding the php connector and an api so I can just import the connect.php file and that will establish a connection according to the pre-configuredvariables and then we will import a close.php file later to end the connection and a check-email.php file along with a insert-name-email-map.php file if the check-email.php page returns true

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php 
+
+	$username = 'root';
+	$password = 'root';
+	$hostname = '196.162.0.1';
+	$domain_url = 'http://leedle.us';
+
+	$connect = new mysqli($username,$password,$hostname, $domain_url);
+
+
+?>
---
+++ email-check.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>email...</title>
+		<?php include 'connect.php'; ?>
+	</head>
+	<body>
+		<div class="body-container">
+			<header>
+				
+			</header>
+
+			<script type="text/javascript">
+
+
+
+				function isUsed (email) {
+
+					// establish php connection
+					// query all the emails
+					// if emails passes through the 
+					// whole sql loop, we can insert it 
+
+				}
+
+				var alertMsg = 'thanks!';
+				var email = document.getElementById('email');
+				if (email.value.length < 5 || !(email.includes('.')) {
+					alertMsg = 'that email didnt work, you could try another one or maybe type that one in once more?';
+				
+				}
+
+				window.alert(alertMsg);
+			
+			</script>	
+
+			<footer>
+
+			</footer>
+		</div>
+	<body>
+</html>
