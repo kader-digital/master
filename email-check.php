5961eb88cd1e71a072b4f18c4d2cc21b196a5d8c
rgbkad <kaderarnold@icloud.com>
2020-07-12 12:13:40 -0400

    fixing up the functions to use the object-oriented implementations of php and to be sure that our email checking is successful in addition to our alert() if the connection could not be established and our insertion function for adding an email and a fullname to the db database

--- email-check.php
+++ email-check.php
@@ -10,16 +10,10 @@
 
 		$link = new mysqli($serveraddr,$username,$password,$dbname);
 
-		if (!$link) {
+		if ($link->connect_error) {
 		
 			echo "<script type='text/javascript'>document.alert('coulnd establish a connection with mysql...');</script>";	
-		
-		}
-
-		else {
-
-			echo "<script type='text/javascript'>document.alert(mysqli_get_host_info($link);</script>";
-		
+			die('connection failed. ');
 		
 		}
 
@@ -27,49 +21,36 @@
 
 	function close () {
 
-		mysqli_close($link);
+		return $link -> $close();
 
 	}
 
-	// so now literally all we have to do is query mysql using the $link object from out connection and test our post objects against the other submitted fields in an array
-	// so lets make an array that contains all of the mysql database table's email fields  
-
-	// once these are defined they
-	// are GLOBAL VARIABLES
-	$email = $_POST['email'];
-	$fullname = $_POST['fullname'];
-
-	// implies the database is named "db"
-	// and all the tables in db are invoked 
-	// with the * operator 
-	function getEmails () {
 	
-		// query db (MariaDB mysql to return
-		// a list of every email in the database
+	function checkEmail () {
+	
 		$sql = "show db customers email from *;";
-		return $emailList = $result -> $mysqli -> $query(sql);
+		$eList = $link -> $query(sql); 
+		$email = $_POST['email'];
 	
-	}
-	
-	function checkEmail (email, eList) {
-	
-	
-		for (var i = 0; i < eList.length - 1; i++) {
-			e = eList[i];
-			if (e == email) {
-				echo "<script type='text/javascript'>window.alert('emails already in the db!');</script>";
-				return;
+		for (var i = 0; i < $eList.length - 1; i++) {
+			$e = $eList[i];
+			if ($e == $email) {
+				return false;
 			}	
 		}	
+
 		return true;
 	}
 
-	function insertValues (email, fullname) {	
+	function insertValues ($email, $fullname) {	
 	
-	$sql = "INSERT VALUES(".$email.".','.".$fullname.")";	
-	return $query($sql);
+		$sql = "INSERT INTO customers ('fullname', 'email')
+		VALUES(".$email.",".$fullname".)";	
+		return $link->query($sql);
 
 
 	}
+
+
 
 ?>
