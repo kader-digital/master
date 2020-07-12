590bfe62994e152e636042afe61823dbb1910833
rgbkad <kaderarnold@icloud.com>
2020-07-12 06:25:01 -0400

    added comments and invoke close.php at the end of our email checking and sql invocation (via the php connector that gets called on submit origionally from home.html)

--- email-check.php
+++ email-check.php
@@ -3,16 +3,30 @@
 // so now literally all we have to do is query mysql using the $link object from out connection and test our post objects against the other submitted fields in an array
 // so lets make an array that contains all of the mysql database table's email fields  
 
+// once these are defined they
+// are GLOBAL VARIABLES
 $email = $_POST['email'];
 $fullname = $_POST['fullname'];
 
-$sql = "show db customers email *;";
+// implies the database is named "db"
+// and all the tables in db are invoked 
+// with the * operator 
+$sql = "show db customers email from *;";
+
+// makes the query using $query()
 $emailList = $result -> $mysqli -> $query($sql);
 
+// makes sure the email isnt already in the 
+// database (db) before it inserts a new 
+// associative array ['email':'fullname'] => ['name':'key']
 for (i = 0; i < emailList.length; i++) {
 
+	// each element of the database's email list
+	// and its corresponding length
+	// gets compared to $email
 	var e  = emailList[i];	
 	var l = e.length;
+
 	var subEmail = ".$email.";
 	if (subEmail.length == l) {
 	
@@ -21,14 +35,19 @@ for (i = 0; i < emailList.length; i++) {
 			".window.alert('email is already in the database. we sent you an email just to reconfirm.');"
 		
 		}
-		// else add the email and fullname to the database tables empty fields
-		// make a new customer and feed it VALUES($fullname, $email)
+		else {
+		
+			// here is where to insert the 
+			// new fields for email and fullname
+			$sql = "INSERT VALUES(".$email.".','.".$fullname.")";	
+			$response = $query($sql);
+			import 'close.php';
+		    	
+		
+		}
+
 	}
 	
-
 }
-
-// this call is in between importing 'connect.php' and importing 'close.php'
-// so we just have to query the (assumingly open) database 
 
 ?>
