07abc526cd8d3ad7a0d7fff26dba0de334ea6003
rgbkad <kaderarnold@icloud.com>
2020-07-12 03:35:36 -0400

    i just have to tie the .php files together and maybe utilize some php functions to provide for neccesary abstractions where needed (where i instead used seperate files that are imported based off of javascript if statements). I have to make sure the php connector works correctly, and that we have the needed error checking to make sure the site isnt hackable and then to write a function which queries the database and checks an email to see if it is already on the list. I am not sure how we can show all the instances in a running database via php,

--- email-check.php
+++ email-check.php
@@ -1,43 +1,8 @@
-<!DOCTYPE html>
+<?php 
 
-<html>
-	<head>
-		<title>email...</title>
-		<?php include 'connect.php'; ?>
-	</head>
-	<body>
-		<div class="body-container">
-			<header>
-				
-			</header>
+	// recieve all the email objects from mysql
+	// run the potential email against all the email
+// objects and see if any have the same name
+// if there is a duplicate, show an alert, if there is not, call insert pair
 
-			<script type="text/javascript">
-
-
-
-				function isUsed (email) {
-
-					// establish php connection
-					// query all the emails
-					// if emails passes through the 
-					// whole sql loop, we can insert it 
-
-				}
-
-				var alertMsg = 'thanks!';
-				var email = document.getElementById('email');
-				if (email.value.length < 5 || !(email.includes('.')) {
-					alertMsg = 'that email didnt work, you could try another one or maybe type that one in once more?';
-				
-				}
-
-				window.alert(alertMsg);
-			
-			</script>	
-
-			<footer>
-
-			</footer>
-		</div>
-	<body>
-</html>
+?>
---
+++ insert-pair.php
@@ -0,0 +1,10 @@
+/* 
+ *
+ *  Gotta brush up on our php js integration strategy here, 
+ *  This will simply add the email and the entered name to 
+ *  the mysql database (after it was checked)
+ *
+ *
+ *
+ * */
+<?php$sql = "INSERT INTO TABLE VALUES (".?><script type='text/javascript'>document.getElementById('email').value; <?php.",".?>document.getElementById('fullname').value;</script><?php.");"?>
